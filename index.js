00d750b802c074ae01a246649642e2d83504a7a7
Hyuugo <rudnov.d@gmail.com>
2018-08-14 13:41:05 +0500

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.get('/search', function (req, res, next) {
     if (req.query.movie_name) {
         if (queryStr.length > 0)
             queryStr += 'AND ';
-        queryStr += 'title SIMILAR TO \'%' + req.query.movie_name + '%\' ';
+        queryStr += 'title ILIKE \'%' + req.query.movie_name + '%\' ';
     }
 
     if (req.query.countries) {
